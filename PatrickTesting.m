d245698d57bfc03d964c1f7f241ff035a2866f79
Patrick2243 <patrick.p.hore@student.uts.edu.au>
2023-10-12 19:05:34 +1100

    Testing Environment, WIll need work on the look

--- PatrickTesting.m
+++ PatrickTesting.m
@@ -1,1 +1,6 @@
 %%
+%Setting up enviroment 
+% Define the ENVIRONMENTXYZ position.
+ENVIRONMENTXYZ = [ 0,0,0];
+% Call the PlaceObject function to place the table.
+PlaceObject('ENVIRONMENT.ply',ENVIRONMENTXYZ);
