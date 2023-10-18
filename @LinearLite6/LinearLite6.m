b852c07cb748f9145dc7d7db300d41d7f4809309
NicholasMullan <125779088+NicholasMullan@users.noreply.github.com>
2023-10-18 16:41:09 +1100

    LinearLite6ModelComplete

--- @LinearLite6/LinearLite6.m
+++ @LinearLite6/LinearLite6.m
@@ -3,7 +3,7 @@ classdef LinearLite6 < RobotBaseClass
     properties(Access = public)              
         plyFileNameStem = 'LinearLite6';
     end
-    
+
     methods
 %% Define robot Function 
         function self = LinearLite6(baseTr)
@@ -21,23 +21,22 @@ classdef LinearLite6 < RobotBaseClass
             % Create the Robot2 model mounted on a linear rail
             link(1) = Link([pi 0 0 pi/2 1]); % PRISMATIC Link
             link(2) = Link([0 0.3165 0 0 0]);
-            % link(3) = Link([0 0.35 pi/2 0 0]);
-            % link(4) = Link([0      0.1357  0.425   -pi     0]);
-            % link(5) = Link([0      0.1197  0.39243 pi      0]);
-            % link(6) = Link([0      0.093   0       -pi/2   0]);
-            % link(7) = Link([0      0       0       0       0]);
+            link(3) = Link([0 0.2 0 0 0]);
+            link(4) = Link([0 0 0 pi/2 0]);
+            link(5) = Link([0 0 -0.1 0 0]);
+            link(6) = Link([0 0 0 -pi/2 0]);
+            link(7) = Link([0 -0.339 0.013 0 0]);
             
             % Incorporate joint limits
             link(1).qlim = [-0.8 -0.01];
             link(2).qlim = [-360 360]*pi/180;
-            % link(3).qlim = [-150 150]*pi/180;
-            % link(4).qlim = [-3.5 300]*pi/180;
-            % link(5).qlim = [-360 360]*pi/180;
-            % link(6).qlim = [-124 124]*pi/180;
-            % link(7).qlim = [-360 360]*pi/180;
+            link(3).qlim = [-150 150]*pi/180;
+            link(4).qlim = [-3.5 300]*pi/180;
+            link(5).qlim = [-360 360]*pi/180;
+            link(6).qlim = [-124 124]*pi/180;
+            link(7).qlim = [-360 360]*pi/180;
 
-            % link(3).offset = -pi/2;
-            % link(5).offset = -pi/2;
+            
         
                       
             self.model = SerialLink(link,'name',self.name);
