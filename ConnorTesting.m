2ad6208db00516311126241447ad4d435dda7f98
ConnorKeogh <Connor.keogh@student.uts.edu.au>
2023-10-28 09:26:44 +1100

    Fixed object colour issues

--- ConnorTesting.m
+++ ConnorTesting.m
@@ -4,7 +4,64 @@
 
 
 
-%% Test the creation of arrays
+%% Test the creation of bottles
+bottleTypes = {
+                'CanYellow.ply';
+                'CanRed.ply';
+                'CanBlue.ply';
+                'CanGreen.ply';
+                };
+
+StartingX = 0;
+TallBarTable = 0.05;
+InitialObjectLocationsArray = [
+                %Location XYZ   
+                StartingX,    -0.4,    TallBarTable;           %Object 1
+                StartingX,    -0.2,   TallBarTable;          %Object 2
+                StartingX,    0,   TallBarTable;          %Object 3
+                StartingX,    0.2,    TallBarTable;          %Object 4
+            ];
+
+for i = 1:size(bottleTypes,1)
+    BottleArray{i} = IR_Object([bottleTypes{i}],['Bottle', num2str(i)], InitialObjectLocationsArray(i,:)); 
+end
+
+
+
+%% 
+clc;
+clf;
+clear all;
+
+%% 
+hold on
+
+name = 'CanYellow.ply';
+%plydata_ = plyread(name);
+%[faceData, vertexData] = plyread(name, 'tri');
+
+
+[faceData, vertexData, plyData{1}] = plyread([name], 'tri');
+%vertex_colors = [plydata.vertex.red, plydata.vertex.green, plydata.vertex.blue];
+
+vertexColours = [plyData{1}.vertex.red ...
+                 , plyData{1}.vertex.green ...
+                 , plyData{1}.vertex.blue]/255;
+                    
+
+link1 = Link('alpha',0,'a',0,'d',0,'offset',0)
+model1 = SerialLink(link1,'name','test');
+
+%link1.Children.FaceVertexCData = vertexColours;
+
+%axis equal;
+%patch('Vertices', vertexData, 'Faces', faceData, 'FaceVertexCData', vertex_colors, ...
+    %'FaceColor', 'flat', 'EdgeColor', 'none');
+% xlabel('X');
+% ylabel('Y');
+% zlabel('Z');
+% title('Colored PLY Object');
+% view(3);
 
 
 %% Test room creation
