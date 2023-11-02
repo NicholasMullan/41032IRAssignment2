d1099918c0b7a5be07ea83fb91f5e751d3283fc0
Patrick2243 <patrick.p.hore@student.uts.edu.au>
2023-11-02 17:00:35 +1100

    E stop implemtented without a while statement

--- eStop.m
+++ eStop.m
@@ -1,51 +1,47 @@
-% classdef eStop < handle
-%     properties
-%         isPressed = false;
-%         option = 0;
-%     end
-% 
-%     methods
-%         function self = eStop()
-% 
-%         end
-% 
-% 
-% 
-%         function press(self)
-% 
-%             if self.isPressed == true
-%                 GUIestop = eStopGUI();
-%                 drawnow();
-% 
-%                 while self.isPressed == true
-%                     pause(0.001);
-%                     if self.isPressed == GUIestop.Continue
-%                         fprintf('\nContinue selected')
-%                         self.option = 1;
-%                         self.isPressed = false;
-%                     elseif self.isPressed == GUIestop.ResetUR3
-%                         fprintf('\nReset UR3 selected')
-%                         self.option = 2;
-%                         self.isPressed = false;
-%                     elseif self.isPressed == GUIestop.ControlUR3
-%                         fprintf('\nControl UR3 selected')
-%                         self.option = 3;
-%                         self.isPressed = false;
-%                     elseif self.isPressed == GUIestop.ResetLinearLite6
-%                         fprintf('\nReset LinearLite6 selected')
-%                         self.option = 4;
-%                         self.isPressed = false;
-%                     elseif self.isPressed == GUIestop.ControlLinearLite6
-%                         fprintf('\nControl LinearLite6 selected')
-%                         self.option = 5;
-%                         self.isPressed = false;
-%                     end
-% 
-% 
-%                 end
-%                 delete(GUIestop);
-%             end
-% 
-%         end
-%     end
-% end
+classdef eStop < handle
+    properties
+        EStopPressed = false;
+        option = 0;
+        GUIestop = []; % Store the GUI handle
+    end
+
+    events
+        ContinuePressed
+    end
+
+    methods
+        function self = eStop()
+            % Constructor (if needed)
+            % Initialize properties or perform setup here
+        end
+
+        function press(self)
+            if self.EStopPressed
+                if isempty(self.GUIestop)
+                    self.GUIestop = eStopGUI();
+                    drawnow();
+                end
+
+                % Set up a listener to watch for the "Continue" button press
+                setappdata(self.GUIestop, 'EStopListener', addlistener(self.GUIestop, 'ContinuePressed', @(src, event) self.onContinuePressed()));
+
+                % Don't delete the GUI here, let it stay open
+                % until the user selects an option in the GUI
+            end
+        end
+
+        function onContinuePressed(self)
+            if ~isempty(self.GUIestop)
+                selectedOption = self.GUIestop.getSelectedOption(); % Get the user's selection from the GUI
+
+                if selectedOption == 1
+                    fprintf('\nContinue selected');
+                    self.option = 1;
+                end
+
+                delete(self.GUIestop);
+                self.GUIestop = []; % Reset the GUI handle
+            end
+        end
+   end
+end
